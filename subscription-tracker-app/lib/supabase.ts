2bcf562e06170890fddf83a3c7cf692417f21c56
hampussellden <hampussellden1@gmail.com>
2023-09-07 10:38:07 +0200

    initial commit
    added basic user management

---
+++ subscription-tracker-app/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://mrmaxsekjsklawiweljv.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ybWF4c2VranNrbGF3aXdlbGp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzU0NzYsImV4cCI6MjAwOTY1MTQ3Nn0.gmpOSmIdgLAYLg6R4g2AuTRLV9a9JzLDr3WpyKUQ5WE";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
