38e26160301f91d3f8c6844b05ffe512885cc41c
hampussellden <hampussellden1@gmail.com>
2023-09-20 10:25:08 +0200

    add global style file

---
+++ subscription-tracker-app/src/style.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+const darkMode = false; //subject to change
+const S = StyleSheet.create({
+  primaryColor: {
+    backgroundColor: darkMode ? "#a67d7d" : "#9ddef8",
+  },
+});
+
+export default S;
